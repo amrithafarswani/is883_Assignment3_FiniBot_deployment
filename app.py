12f4d6875baab5e0e820a2472986d175784d26ad
amrithafarswani <122059440+amrithafarswani@users.noreply.github.com>
2023-11-25 16:05:13 -0500

    Update app.py

--- app.py
+++ app.py
@@ -11,35 +11,10 @@ import pandas as pd
 open_AI_key = os.environ.get('OPENAI_API_KEY')
 openai.api_key = open_AI_key
 
-
 ### Here, with some adjustments, copy-paste the code you developed for Question 1 in Assignment 3 
 ##########################################################################
-# Function to perform FiniBot analysis and generate recommendation
 
-# Streamlit UI
-def main():
-    # Header
-    st.header("Welcome to FiniBot Services!")
-    st.write("Please upload your spreadsheet using the button below.")
-    st.write("Download the spreadsheet template [here](link_to_template)")
 
-    # File uploader
-    uploaded_file = st.file_uploader("Upload spreadsheet", type=["csv", "xlsx"])
-
-    # Radio button for user type
-    user_type = st.radio("Select your expertise level:", ('Novice', 'Expert'))
-
-    # Display uploaded spreadsheet
-    if uploaded_file is not None:
-        df = pd.read_csv(uploaded_file)  # You can modify this based on the file type
-        st.dataframe(df)
-
-        # Perform analysis and display recommendation
-        analysis_result = perform_analysis(df, user_type)
-        st.markdown(f"## FiniBot's Analysis and Recommendation\n{analysis_result}")
-
-if __name__ == '__main__':
-    main()
 
 
 ##########################################################################
@@ -47,5 +22,26 @@ if __name__ == '__main__':
 
 # UX goes here. You will have to encorporate some variables from the code above and make some tweaks.
 
+# Streamlit UI
+
+# Header
+st.header("Welcome to FiniBot Services!")
+st.write("Please upload your spreadsheet using the button below.")
+st.write("Download the spreadsheet template [here](link_to_template)")
+
+# File uploader
+uploaded_file = st.file_uploader("Upload spreadsheet", type=["csv", "xlsx"])
+
+# Radio button for user type
+user_type = st.radio("Select your expertise level:", ('Novice', 'Expert'))
+
+# Display uploaded spreadsheet
+if uploaded_file is not None:
+   df = pd.read_csv(uploaded_file)  # You can modify this based on the file type
+   st.dataframe(df)
+
+    # Perform analysis and display recommendation
+        analysis_result = perform_analysis(df, user_type)
+        st.markdown(f"## FiniBot's Analysis and Recommendation\n{analysis_result}")
 
 
